0972d087a70807f8c7e832e9431c010bbb20f04b
Shrutikarmakar15 <shrutikarmakar2002@gmail.com>
2025-08-03 13:58:11 +0530

    Big Product

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import Sidebar from './components/side-nav';
+// import { Card } from 'react-bootstrap';
 import Navbar from './components/navbar';
-import Dashboard from './pages/dashboard';
-import './App.css';
+import BigProduct from './pages/module/big-product/bigProduct';
+import BigProductAdd from './pages/module/big-product/bigProductAdd';
+import BigProductEdit from './pages/module/big-product/bigProductEdit';
+import { Routes , Route } from 'react-router-dom';
+import BigProductView from './pages/module/big-product/bigProductView';
+
+// import Sidebar from './components/side-nav';
+// import Navbar from './components/navbar';
+// import Dashboard from './pages/dashboard';
+// import './App.css';
 
 function App() {
   return (
-    <div className="app-container">
-      <Sidebar />
+    <div >
+      {/* <Sidebar />
       <div className="main-content">
         <Navbar />
         <Dashboard />
-      </div>
+      </div> */}
+      <Navbar/>
+       {/* <BigProductAdd/> */}
+      {/* <BigProduct/> */}
+      {/* <BigProductEdit/> */}
+    
+        <Routes>
+          <Route path="/" element={<BigProduct/>} />
+          <Route path="/bigProductAdd" element={<BigProductAdd/>} />
+          
+          <Route path="/bigProductEdit" element={<BigProductEdit/>} />
+          <Route path="/bigProductView" element={<BigProductView/>} />
+        </Routes>
+      
+     
+
+      
     </div>
   );
 }
---
+++ src/components/cards/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+      hay
+    </div>
+  )
+}
+
+export default card
