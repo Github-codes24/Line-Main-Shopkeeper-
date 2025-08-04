9bb5c845b004106840cae7888c62d54894cc6a66
kajalkachhwaha91 <kajalkachhwaha91@gmail.com>
2025-08-04 10:24:18 +0530

    Updated Tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import Sidebar from './components/side-nav';
-import Navbar from './components/navbar';
-import Dashboard from './pages/dashboard';
+import Sidebar from './components/layout/sidebar';
+import Navbar from './components/layout/navbar';
+// import Dashboard from './pages/dashboard';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="app-container">
-      <Sidebar />
-      <div className="main-content">
-        <Navbar />
-        <Dashboard />
-      </div>
-    </div>
+    <div className="App">
+     
+     <BrowserRouter>
+      <Routes>
+       
+        {/* <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/order-management" element={<Navbar />} /> */}
+        {/* <Route path="/sidebar" element={<Sidebar />} /> */}
+        {/* <Route path="/top-worker" element={<TopWorker />} /> */}
+      </Routes>
+    </BrowserRouter>
+
+  </div>
   );
 }
 
