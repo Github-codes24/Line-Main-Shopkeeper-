4a5be74d31b7523a06ca969c23ca7dd089eb8470
vikash soni <vikashsoni651@gmail.com>
2025-08-07 01:05:11 +0530

    my current work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import Sidebar from './components/side-nav';
-import Navbar from './components/navbar';
+//import Sidebar from './components/side-nav';
+import  Navbar  from './components/navbar'
 import Dashboard from './pages/dashboard';
+import SmallProduct from './pages/smallproduct';
 import './App.css';
 
 function App() {
   return (
     <div className="app-container">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="main-content">
         <Navbar />
         <Dashboard />
+        <SmallProduct />
       </div>
     </div>
   );
