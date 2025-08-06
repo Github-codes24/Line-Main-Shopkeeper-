a5b4a7e1731ca73fe61a6cb9dcaed084b162a4e5
Delta student <149225920+Tushar123097@users.noreply.github.com>
2025-08-06 13:47:11 +0530

    Update App.js and payment-process.jsx before branch switch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import PaymentProcess from "./pages/module/payment/payment-process";
 import Payment from "./pages/module/payment/payment";
+// import PaymentProcess from "./pages/module/payment/payment-process";
 function App(){
   return (
     <div>
       {/* <PaymentProcess/> */}
       <Payment/>
+
     </div>
   )
 }
