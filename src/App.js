9156b821bf4cd91bfbaa0789bc3ef2fe98eacfb6
vikash soni <vikashsoni651@gmail.com>
2025-08-07 16:40:16 +0530

    done small product pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 // import { Card } from 'react-bootstrap';
 import Navbar from './components/navbar';
-import BigProduct from './pages/module/big-product/bigProduct';
-import BigProductAdd from './pages/module/big-product/bigProductAdd';
-import BigProductEdit from './pages/module/big-product/bigProductEdit';
+// import BigProduct from './pages/module/big-product/bigProduct';
+// import BigProductAdd from './pages/module/big-product/bigProductAdd';
+// import BigProductEdit from './pages/module/big-product/bigProductEdit';
 import { Routes , Route } from 'react-router-dom';
-import BigProductView from './pages/module/big-product/bigProductView';
+// import BigProductView from './pages/module/big-product/bigProductView';
+
+import SmallProduct from './pages/module/small-product/smallProduct';
+import SmallProductAdd from './pages/module/small-product/smallProductAdd';
+import SmallProductEdit from './pages/module/small-product/smallProductEdit';
+import SmallProductView from './pages/module/small-product/smallProductView';
+
 
 // import Sidebar from './components/side-nav';
 // import Navbar from './components/navbar';
@@ -26,11 +32,11 @@ function App() {
       {/* <BigProductEdit/> */}
     
         <Routes>
-          <Route path="/" element={<BigProduct/>} />
-          <Route path="/bigProductAdd" element={<BigProductAdd/>} />
+          <Route path="/" element={<SmallProduct/>} />
+          <Route path="/smallProductAdd" element={<SmallProductAdd/>} />
           
-          <Route path="/bigProductEdit" element={<BigProductEdit/>} />
-          <Route path="/bigProductView" element={<BigProductView/>} />
+          <Route path="/smallProductEdit" element={<SmallProductEdit/>} />
+          <Route path="/smallProductView" element={<SmallProductView/>} />
         </Routes>
       
      
